24e2af3aec89b66dd819d4af16f3fc3bd425f4d8
zchang <zchangvx@hotmail.com>
2020-05-23 16:52:49 +0200

    Added bidirectional movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    //Components
+    private Rigidbody2D rb;
+    private BoxCollider2D bc;
+    private SpriteRenderer sr;
+
+    //Movement Settings
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private float jumpForce;
+    [SerializeField]
+
+    //Sprite Settings
+    private Sprite sprite;
+
     void Start()
     {
-        
+        //Create and save component references
+        sr = gameObject.AddComponent<SpriteRenderer>();
+        rb = gameObject.AddComponent<Rigidbody2D>();
+        bc = gameObject.AddComponent<BoxCollider2D>();
+
+        //Setup
+        //TODO: consider setting up component as they are created to declutter code
+
+        //SpriteRenderer settings
+        if(sprite != null) sr.sprite = this.sprite;
+
+        //BoxCollider settings
+        if(sprite != null){
+            //Save old BC settings
+            Destroy(this.bc);
+            bc = gameObject.AddComponent<BoxCollider2D>();
+            //Add old settings to new BC component
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //Movement
+        rb.velocity = new Vector2( Input.GetAxis("Horizontal") * moveSpeed ,rb.velocity.y);
+
     }
 }
