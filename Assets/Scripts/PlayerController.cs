2429703ddd87959573690f4f14b77c252fea89e0
Jesús Chang <zchangvx@hotmail.com>
2020-05-24 04:24:49 +0200

    Dev 002 (#4)
    * Added PlayerController script
    
    * Added bidirectional movement
    
    * Added player jump on Keycode.Space down
    
    * Added blank32 sprite for testing

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    //Components
+    private Rigidbody2D rb;
+    private BoxCollider2D bc;
+    private SpriteRenderer sr;
+
+    //Movement Settings
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private float jumpForce;
+    [SerializeField]
+
+    //Sprite Settings
+    private Sprite sprite;
+
+    void Start()
+    {
+        //Create and save component references
+        sr = gameObject.AddComponent<SpriteRenderer>();
+        rb = gameObject.AddComponent<Rigidbody2D>();
+        bc = gameObject.AddComponent<BoxCollider2D>();
+
+        //Setup
+        //TODO: consider setting up component as they are created to declutter code
+
+        //SpriteRenderer settings
+        if(sprite != null) sr.sprite = this.sprite;
+
+        //BoxCollider settings
+        if(sprite != null){
+            //Save old BC settings
+            Destroy(this.bc);
+            bc = gameObject.AddComponent<BoxCollider2D>();
+            //Add old settings to new BC component
+        }
+    }
+
+    void Update()
+    {
+        //Movement
+        rb.velocity = new Vector2( Input.GetAxis("Horizontal") * moveSpeed ,rb.velocity.y);
+
+        //Jump
+        if(Input.GetKeyDown(KeyCode.Space)){
+            //TODO: create input class
+            rb.AddForce( new Vector2(0, jumpForce) );
+        }
+
+    }
+}
