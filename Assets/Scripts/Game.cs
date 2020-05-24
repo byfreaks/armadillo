efb4a717341e6d0266ff1314a34138aacc5f4320
Jesús Chang <zchangvx@hotmail.com>
2020-05-24 04:27:04 +0200

    Dev 009 (#3)
    * Added GameManagerScript script
    
    * Added Game Instance class
    
    * Delete unnecesary line
    
    playerObj.GetComponent<PlayerControllerScript>() line deleted.
    Component already exists in player prefab

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game
+{
+    //Partida
+    public GameObject playerObj; //Player Preset
+    public GameObject vehicleObj;
+    public Camera cameraObj;
+
+    [Header("Temporal settings")]
+    //Temporal
+    public Sprite testSolidSprite;
+
+    public Game(){
+        playerObj = new GameObject("Player");
+        playerObj.transform.position = new Vector3(0, 0, -10);
+        
+        vehicleObj = new GameObject("TestSolid");
+        vehicleObj.transform.position = new Vector3(0, 10, -10);
+        vehicleObj.AddComponent<SpriteRenderer>().sprite = testSolidSprite;
+        vehicleObj.AddComponent<BoxCollider2D>();
+        vehicleObj.AddComponent<Rigidbody2D>().isKinematic = true;
+
+    }
+}
