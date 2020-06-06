1de107776815a84cb45b9c54b97f823191bd8ca6
Miguel Antonio Peña Fraga <mapena.16@est.ucab.edu.ve>
2020-06-06 01:30:04 -0400

    Se agregó el componente de vida y se creó la configuración adecuada para su uso a través de un editor personalizado

---
+++ Assets/Scripts/Editor/HealthEditor.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Health))]
+[CanEditMultipleObjects]
+public class HealthEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        Health health = (Health)target;
+
+        EditorGUILayout.Space();
+        health.HealthPoints = EditorGUILayout.IntField("Health points", health.HealthPoints);
+        health.MaxHealthPoints = EditorGUILayout.IntField("Max health points", health.MaxHealthPoints);
+        health.MinHealthPoints = EditorGUILayout.IntField("Min health points", health.MinHealthPoints);
+        health.IsAlive = EditorGUILayout.Toggle("Is alive", health.IsAlive);
+        EditorGUILayout.Space(30);
+
+        EditorGUILayout.LabelField("Actions");
+        if(GUILayout.Button("Increment health points (+1)"))
+        {
+            health.incrementHealthPoints(1);
+        }
+
+        if(GUILayout.Button("Decrement health points (-1)"))
+        {
+            health.decrementHealthPoints(1);
+        }
+
+        if(GUILayout.Button("Kill entity"))
+        {
+            health.HealthPoints = 0;
+        }
+
+        if(GUILayout.Button("Set max health Points"))
+        {
+            health.HealthPoints = health.MaxHealthPoints;
+        }
+
+        if(GUILayout.Button("Set min health Points"))
+        {
+            health.HealthPoints = health.MinHealthPoints;
+        }
+        
+        serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class Health : MonoBehaviour
+{
+    private int healthPoints;
+    private int maxHealthPoints;
+    private int minHealthPoints;
+    private bool alive;
+
+    private void Awake() {
+        this.IsAlive = true;
+        this.HealthPoints = 100;
+        this.MaxHealthPoints = 100;
+        this.MinHealthPoints = 10;
+    }
+
+    public int HealthPoints{
+        get { return healthPoints; }
+        set { healthPoints = value; }
+    }
+
+    public bool IsAlive{
+        get { 
+            return this.HealthPoints <= 0 ? false:true;
+        }
+        set { alive = value; }
+    }
+
+    public int MaxHealthPoints{
+        get { return maxHealthPoints; }
+        set { maxHealthPoints = value; }
+    }
+
+    public int MinHealthPoints{
+        get { return minHealthPoints; }
+        set { minHealthPoints = value; }
+    }
+
+    public void incrementHealthPoints(int points){
+        this.HealthPoints = this.HealthPoints + points;
+    } 
+
+    public void decrementHealthPoints(int points){
+        this.HealthPoints = this.HealthPoints - points;
+    }
+}
