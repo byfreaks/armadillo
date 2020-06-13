2d5ceb533d2bce2fbb323b0effd91cdf0609d2e4
Miguel Antonio Peña Fraga <mapena.16@est.ucab.edu.ve>
2020-06-13 02:16:57 -0400

    Se realizó el menu principal del juego con un apartado de controles y opciones de inicio y cierre del juego

---
+++ Assets/Scripts/Controllers/UIMenuManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class UIMenuManager : MonoBehaviour
+{
+
+    public GameObject optionsPanel;
+    public GameObject controlsPanel;
+    public string firstScene;
+
+    void Awake()
+    {
+        this.optionsPanel.SetActive(true);
+        this.controlsPanel.SetActive(false);        
+    }
+
+    public void moveTo(GameObject panel){
+        this.closeAllPanels();
+        panel.SetActive(true);
+    }
+
+    public void closeGame(){
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    public void startGame(){
+        SceneManager.LoadScene(firstScene);
+    }
+
+    private void closeAllPanels(){
+        this.optionsPanel.SetActive(false);
+        this.controlsPanel.SetActive(false); 
+    }
+}
