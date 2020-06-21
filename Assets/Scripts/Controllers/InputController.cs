d557c074e451047a0a483027668d51aec2509c8c
Miguel Antonio Peña Fraga <mapena.16@est.ucab.edu.ve>
2020-06-21 03:39:24 -0400

    Input controller creado

---
+++ Assets/Scripts/Controllers/InputController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.LeftKey();
+    }
+
+    public bool LeftKey(){
+        if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)){
+           Debug.Log("Left Key");
+           return true; 
+        }else{
+            return false;
+        }
+    }
+}
