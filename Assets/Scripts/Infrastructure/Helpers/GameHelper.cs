c1b4e80e088c04b49d668d5c493fa1768f734954
zchang <zchangvx@hotmail.com>
2020-07-23 04:09:49 +0200

    [dev-037] Cached values so they are saved only once

--- Assets/Scripts/Infrastructure/Helpers/GameHelper.cs
+++ Assets/Scripts/Infrastructure/Helpers/GameHelper.cs
@@ -4,7 +4,24 @@ using UnityEngine;
 
 public static class GameHelper
 {
-    public static GameManagerScript GameManager { get => GameObject.Find("GameManager").GetComponent<GameManagerScript>(); }
-    public static PlayerController Player {get => GameObject.Find("Player").GetComponent<PlayerController>(); }
+    private static GameManagerScript gameManager;
+    public static GameManagerScript GameManager { get => gameManager ?? GetGameManager(); set => gameManager = value; }
+    
+    private static PlayerController player;
+    public static PlayerController Player { get => player ?? GetPlayerController(); set => player = value; }
 
+    private static GameManagerScript GetGameManager(){
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManagerScript>();
+        return gameManager;
+    } 
+
+    private static PlayerController GetPlayerController(){
+        player = GameObject.Find("Player").GetComponent<PlayerController>();
+        return player;
+    }
+
+    public static void ClearCache(){
+        gameManager = null;
+        player = null;
+    }
 }
