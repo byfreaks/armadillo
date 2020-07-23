37c37c5c1dc87219f72752abdff8ac7755679eb6
zchang <zchangvx@hotmail.com>
2020-07-23 04:16:29 +0200

    [dev-037] Shotened GameHelper script

--- Assets/Scripts/Infrastructure/Helpers/GameHelper.cs
+++ Assets/Scripts/Infrastructure/Helpers/GameHelper.cs
@@ -6,19 +6,11 @@ public static class GameHelper
 {
     private static GameManagerScript gameManager;
     public static GameManagerScript GameManager { get => gameManager ?? GetGameManager(); set => gameManager = value; }
+    private static GameManagerScript GetGameManager(){ return gameManager = GameObject.Find("GameManager").GetComponent<GameManagerScript>(); }
     
     private static PlayerController player;
     public static PlayerController Player { get => player ?? GetPlayerController(); set => player = value; }
-
-    private static GameManagerScript GetGameManager(){
-        gameManager = GameObject.Find("GameManager").GetComponent<GameManagerScript>();
-        return gameManager;
-    } 
-
-    private static PlayerController GetPlayerController(){
-        player = GameObject.Find("Player").GetComponent<PlayerController>();
-        return player;
-    }
+    private static PlayerController GetPlayerController(){ return player = GameObject.Find("Player").GetComponent<PlayerController>(); }
 
     public static void ClearCache(){
         gameManager = null;
