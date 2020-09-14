9bef2cd1a7b0314cfc722d11d3bb4056c4aae4af
zchang <zchangvx@hotmail.com>
2020-09-14 04:05:45 +0200

    [DEV-055] Added parallax background

---
+++ Assets/Scripts/Controllers/BackgroundController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    public List<LoopableBackground> Backgrounds = new List<LoopableBackground>();
+
+    void Start()
+    {
+        var pos = 1;
+        foreach(LoopableBackground background in Backgrounds){
+            var obj = GameObject.CreatePrimitive(PrimitiveType.Quad);
+            obj.name = background.name == "" ? $"Layer {background.order??pos}" : background.name;
+            obj.transform.SetParent(this.transform);
+            obj.AddComponent<LoopableBackgroundController>().Build(background, pos++);
+        }
+    }
+}
---
+++ Assets/Scripts/Controllers/LoopableBackgroundController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopableBackgroundController : MonoBehaviour
+{
+    private LoopableBackground background;
+
+    private Material material;
+    private MeshRenderer meshRenderer;
+
+    public void Build(LoopableBackground bg, int pos){
+        material = new Material(Shader.Find("Unlit/Transparent"));
+        material.mainTexture = bg.texture.texture;
+
+        gameObject.GetComponent<MeshRenderer>().material = material;
+        gameObject.transform.position = Camera.main.transform.position;
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, bg.order??pos );
+        gameObject.transform.localScale = new Vector3(20, 12, 1); //FIXME: Hardcode
+
+        background = bg;
+    }
+
+    private void Update() {
+        if(background!=null)
+            material.mainTextureOffset += background.offset * Time.deltaTime;
+    }
+}
