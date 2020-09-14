f3bb252e74f9737e337b53da0597ec9717f406e5
zchang <zchangvx@hotmail.com>
2020-09-14 20:01:38 +0200

    [DEV-055] Added simple background gradient

--- Assets/Scripts/Controllers/BackgroundController.cs
+++ Assets/Scripts/Controllers/BackgroundController.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class BackgroundController : MonoBehaviour
 {
     public List<LoopableBackground> Backgrounds = new List<LoopableBackground>();
+    public Sprite backgroundGradient;
+    public Vector2 backgroundGradientScale;
 
     void Start()
     {
+        if(backgroundGradient!=null){
+            var obj = new GameObject("BackgroundGradient");
+            obj.transform.SetParent(this.transform);
+            obj.AddComponent<SpriteRenderer>().sprite = this.backgroundGradient;
+            obj.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, 50);
+            var baseScale = new Vector2(4,1);
+            obj.transform.localScale = baseScale * backgroundGradientScale;
+        }
+
         var pos = 1;
         foreach(LoopableBackground background in Backgrounds){
             var obj = GameObject.CreatePrimitive(PrimitiveType.Quad);
